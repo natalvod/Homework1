818e4dd53ec6da1e2222325643131c596aaf4b1c
Nataiia Vodopianova <natalvod@yandex.ru>
2023-02-12 02:48:16 +0300

    added code play2 in index.html and code js

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ function seasonPlay() {
     }
 };
 
+function memoryFruit() {
+    let fruit = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    fruit = fruit.sort(() => Math.random() - 0.5);
+    alert(fruit);
+    let userAnswer1 = prompt('Чему равнялся первый элемент массива?');
+    let userAnswer2 = prompt('Чему равнялся последний элемент массива?');
+    let answer1 = fruit[0];
+    let answer2 = fruit[fruit.length - 1];
+    if (userAnswer1.toLowerCase() === answer1.toLowerCase() && userAnswer2.toLowerCase() === answer2.toLowerCase()) {
+        alert('Вы угадали! У Вас отличная память!');
+    } else if (userAnswer1.toLowerCase() !== answer1.toLowerCase() && userAnswer2.toLowerCase() !== answer2.toLowerCase()) {
+        alert('Вы не угадали! Попробуйте еще раз!');
+    } else {
+        alert('Вы были близки к победе!');
+    }
+};
